db4a54e76b400b7c0f5dbe90087fc3a98268412b
vadim-voloshchuk <kingofsweetsx2k@gmail.com>
2025-03-12 12:46:02 +0300

    -molu work

--- src/Pages/MyCourses/MyCourses.tsx
+++ src/Pages/MyCourses/MyCourses.tsx
@@ -9,49 +9,64 @@ import styles from './styles.module.css';
 import NewCourse from '@/Components/ElementUi/NewCourse/NewCourseCard';
 import { Link } from 'react-router-dom';
 
+// Определяем интерфейс Course с ограниченным набором значений для status
+interface Course {
+  id: number;
+  title: string;
+  description: string;
+  status: 0 | 1 | 2;
+  img: string;
+}
+
 const MyCoursesPage: React.FC = () => {
-  const [courses] = useState([
+  const [courses] = useState<Course[]>([
     {
       id: 1,
       title: "Основы Python: Путь от новичка к разработчику",
-      description: "Этот курс поможет вам освоить базовые концепции Python, научиться работать с переменными, циклами, функциями и основами ООП",
+      description:
+        "Этот курс поможет вам освоить базовые концепции Python, научиться работать с переменными, циклами, функциями и основами ООП",
       status: 0,
-      img: pythonCourse
+      img: pythonCourse,
     },
     {
       id: 2,
       title: "Маркетинг в социальных сетях",
-      description: "Научитесь продвигать бизнес в , анализировать эффективность рекламных кампаний и привлекать аудиторию.",
+      description:
+        "Научитесь продвигать бизнес, анализировать эффективность рекламных кампаний и привлекать аудиторию.",
       status: 1,
-      img: marketingCourse
+      img: marketingCourse,
     },
     {
-        id: 3,
-        title: "Искусство публичных выступлений",
-        description: "Разберём, как подготовиться к выступлению, управлять голосом, жестикуляцией и привлекать внимание аудитории",
-        status: 2,
-        img: artCourse
+      id: 3,
+      title: "Искусство публичных выступлений",
+      description:
+        "Разберём, как подготовиться к выступлению, управлять голосом, жестикуляцией и привлекать внимание аудитории",
+      status: 2,
+      img: artCourse,
     },
     {
-        id: 4,
-        title: "Основы Python: Путь от новичка к разработчику",
-        description: "Этот курс поможет вам освоить базовые концепции Python, научиться работать с переменными, циклами, функциями и основами ООП",
-        status: 0,
-        img: pythonCourse
+      id: 4,
+      title: "Основы Python: Путь от новичка к разработчику",
+      description:
+        "Этот курс поможет вам освоить базовые концепции Python, научиться работать с переменными, циклами, функциями и основами ООП",
+      status: 0,
+      img: pythonCourse,
     },
     {
-        id: 5,
-        title: "Маркетинг в социальных сетях",
-        description: "Научитесь продвигать бизнес в , анализировать эффективность рекламных кампаний и привлекать аудиторию.",
-        status: 1,
-        img: marketingCourse
+      id: 5,
+      title: "Маркетинг в социальных сетях",
+      description:
+        "Научитесь продвигать бизнес, анализировать эффективность рекламных кампаний и привлекать аудиторию.",
+      status: 1,
+      img: marketingCourse,
     },
     {
-        id: 6,
-        title: "Маркетинг в социальных сетях",
-        description: "Научитесь продвигать бизнес в , анализировать эффективность рекламных кампаний и привлекать аудиторию.",
-        status: 1,
-        img: marketingCourse
+      id: 6,
+      title: "Маркетинг в социальных сетях",
+      description:
+        "Научитесь продвигать бизнес, анализировать эффективность рекламных кампаний и привлекать аудиторию.",
+      status: 1,
+      img: marketingCourse,
     },
   ]);
 
@@ -61,7 +76,9 @@ const MyCoursesPage: React.FC = () => {
       <div className={styles.containerCoursePage}>
         <h1 className="text-3xl font-bold mb-8">Ваши курсы</h1>
         <CardCourse courses={courses} />
-        <Link className={styles.newCourseLink} to="/"><NewCourse /></Link>
+        <Link className={styles.newCourseLink} to="/">
+          <NewCourse />
+        </Link>
       </div>
       <Footer />
     </>
